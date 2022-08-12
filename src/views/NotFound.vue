c82b3d4a1f4f9bd4ef5c958f112af4eb2ac5085c
DanielMa <mwldaniel_008@126.com>
2022-08-13 06:19:47 +0800

    Error Handling and 404s

---
+++ src/views/NotFound.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Oops!</h1>
+  <h3>The {{ resource }} you're looking for is not here.</h3>
+  <router-link class="go-back-link" :to="{ name: 'EventList' }"
+    >Back to the home page</router-link
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    resource: {
+      type: String,
+      required: true,
+      default: 'page'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.go-back-link {
+  color: #2c3e50;
+  cursor: pointer;
+}
+</style>
